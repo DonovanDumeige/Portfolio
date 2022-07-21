f00a47a57c42e4c00b293848d049f49a3db3bc37
DonovanDumeige <donovan.dumeige@gmail.com>
2022-07-21 17:00:55 +0200

    Portfolio màj 21/07

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,37 @@
 const menu = document.querySelector('#menu');
 
 /* Création des éléments nécessaire */
+
+function create() {
+    const navbar = document.createElement('nav')
+}
 const navbar = document.createElement('nav')
 console.log(menu, navbar);
 
-menu.addEventListener("click",shows)
+menu.addEventListener("click", shows)
+
 function shows() {
-    if(navbar.style.display === "block"){
+    create();
+    if (navbar.style.display === "block") {
         navbar.style.display = "none";
-    } else{
+    } else {
         navbar.style.display = "block";
     }
 }
+
+
+//Typing effects
+
+let txt = "Je m'appelle Donovan et je suis développeur web junior"
+let i = 0
+let speed = 70;
+function Typing() {
+    if (i < txt.length) {
+        document.querySelector(".intro").innerHTML += txt.charAt(i)
+        i++;
+        setTimeout(Typing, speed);
+        intro.append(p)
+    }
+
+}
+window.addEventListener("load", Typing)
