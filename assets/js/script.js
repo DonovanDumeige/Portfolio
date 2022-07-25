4c25013e341b21835433c650e75cc9eb56e1e503
DonovanDumeige <donovan.dumeige@gmail.com>
2022-07-25 17:10:39 +0200

    Portfolio màj 25 juillet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,6 @@
 "use strict"
 
-const menu = document.querySelector('#menu');
-
-/* Création des éléments nécessaire */
-
-function create() {
-    const navbar = document.createElement('nav')
-}
-const navbar = document.createElement('nav')
-console.log(menu, navbar);
-
-menu.addEventListener("click", shows)
-
+//event menu burger
 function shows() {
     create();
     if (navbar.style.display === "block") {
@@ -22,18 +11,52 @@ function shows() {
 }
 
 
-//Typing effects
-
+//typing effects with animate.
 let txt = "Je m'appelle Donovan et je suis développeur web junior"
-let i = 0
+let count = 0
 let speed = 70;
+const blinkBar = document.querySelector('.blink-bar');
+console.log(blinkBar);
 function Typing() {
-    if (i < txt.length) {
-        document.querySelector(".intro").innerHTML += txt.charAt(i)
-        i++;
+    if (count < txt.length) {
+        document.querySelector(".intro p").innerHTML += txt.charAt(count)
+        count++;
         setTimeout(Typing, speed);
-        intro.append(p)
+        
     }
-
 }
-window.addEventListener("load", Typing)
+function blink() { 
+    
+    const keyframes = [
+        {opacity:1},
+        {opacity:0},
+        {opacity:1}
+    ]
+
+    const options = {
+        duration:600,
+        iterations : Infinity
+    }
+    blinkBar.animate(keyframes, options)
+    //moveBar();
+ }; 
+
+/*  function moveBar(){
+    const keyframes =[
+        {left:0},
+        {left:"96%"}
+    ]
+    const options = {
+        duration : 3500,
+        fill:"forwards"
+    }
+    blinkBar.animate(keyframes,options)
+ } */
+
+function init() {
+    Typing();
+    blink();
+}
+window.addEventListener("load", init) 
+
+
