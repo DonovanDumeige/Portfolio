7acfce962d32a005cc0d03c05e179779333e88b7
DonovanDumeige <donovan.dumeige@gmail.com>
2022-07-27 17:00:34 +0200

    Portfolio màj 27 juillet

---
+++ assets/js/carousel-v3.js
@@ -0,0 +1,70 @@
+"use strict"
+export default class Carousel {
+    btns = [];
+    items = [];
+    dots = [];
+    constructor(imgs){
+        // Crée mon carousel.
+        const container = document.querySelector(".photo-folio");
+        const dots = document.createElement("div");
+        dots.classList.add("dots");
+        imgs.forEach((img, i) => {
+            const div = document.createElement("div");
+            div.classList.add("items", "fade");
+            const image = document.createElement("img");
+            image.src = img;
+            div.append(image);
+            const dot = document.createElement("span");
+            dot.classList.add("dot");
+            dot.dataset.id = i;
+            dots.append(dot);
+            container.append(div);
+            this.dots.push(dot);
+            this.items.push(div);
+        })
+        container.append(dots);
+        const next = document.createElement("a");
+        next.classList.add("next");
+        next.innerHTML = "&#10095;";
+        const prev = document.createElement("a");
+        prev.classList.add("prev");
+        prev.innerHTML = "&#10094;";
+        container.append(next, prev);
+        this.btns.push(next, prev);
+        this.container=container;
+    }
+
+    showItems(n) {
+        // Affiche un élément de mon carousel et cache les autres.
+        this.index = n > this.items.length - 1 ? 0 : n < 0 ? this.items.length - 1 : n;
+        this.items.forEach((item, i) => {
+            item.style.display = "none";
+            this.dots[i].classList.remove("active");
+        })
+        this.items[this.index].style.display = "block";
+        this.dots[this.index].classList.add("active");
+    }
+
+    currentItem(e) {
+        // Affiche l'image qui correspond au point.
+        let n = parseInt(e.target.dataset.id);
+        this.showItems(n);
+    }
+
+    changeItem(e){
+        if(e.target.classList.contains("next")){
+            this.showItems(++this.index);
+        }else{
+            this.showItems(--this.index);
+        }
+    }
+
+    init(){
+        // Affiche la première image et ajoute les écouteurs d'évènment.
+        this.showItems(0);
+        this.dots.forEach(dot=>dot.addEventListener("pointerdown", this.currentItem.bind(this)));
+        this.btns.forEach(btn=>btn.addEventListener("pointerdown", this.changeItem.bind(this)));
+    }
+
+
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -12,46 +12,92 @@ function show() {
         navbar.style.display = "none"
         //navbar.style.transform = "translateX(-100%)"
     }
-    
 }
 
+function close() {
+    navbar.style.display = "none"
+}
+//window.addEventListener("click", close) 
 menu.addEventListener("click", show)
 closeMenu.addEventListener("click", show)
-navbar.addEventListener("mouseleave", show)
+
+// cliquer sur body fait disparaitre la navbar
+// J'ai travaillé sur une fonction à part pour éviter l'ouverture de la nav au clic du body.
+
+
+// remplacer hover par un click sur span
+const overlay = document.querySelectorAll('.folio-overlay');
+
+// se déclenche sur chaque folio.overlay
+overlay.forEach(o => {
+    o.addEventListener("click", remplaceHover)
+});
+
+function remplaceHover() {
+    console.log(this);
+    const overSymbol = this.querySelector('h4 .symbol')
+    const overSpan = this.querySelector('h4 .more')
+    //e.stopPropagation(); // permet de ne déclencher que l'event de la cible et pas celui des parents.
+    if (this.style.height === "15%") {
+        this.style.height = "100%"
+        overSymbol.innerHTML = "&#128899;"
+        overSpan.textContent = "voir moins"
+    } else {
+        this.style.height = "15%";
+        overSymbol.innerHTML = "&#128897;"
+        overSpan.textContent = "voir plus"
+    }
+}
+
+
 //typing effects with animate.
-let txt = "Je m'appelle Donovan et je suis développeur web junior"
+let txt = "Je suis Donovan"
 let count = 0
 let speed = 70;
 const blinkBar = document.querySelector('.blink-bar');
-console.log(blinkBar);
+
+//gère l'effet de typing
 function Typing() {
     if (count < txt.length) {
         document.querySelector(".intro p").innerHTML += txt.charAt(count)
         count++;
         setTimeout(Typing, speed);
-        
+
     }
 }
-function blink() { 
-    
-    const keyframes = [
-        {opacity:1},
-        {opacity:0},
-        {opacity:1}
+
+// pour la blinkbar
+function blink() {
+
+    const keyframes = [{
+            opacity: 1
+        },
+        {
+            opacity: 0
+        },
+        {
+            opacity: 1
+        }
     ]
 
     const options = {
-        duration:600,
-        iterations : Infinity
+        duration: 600,
+        iterations: Infinity
     }
     blinkBar.animate(keyframes, options)
- }; 
+};
 
-
+// je démarre à partir d'une fonction init pour plus de simplicité.
 function init() {
     Typing();
     blink();
 }
-window.addEventListener("load", init) 
+window.addEventListener("load", init)
 
+// Second carousel
+import c from "./carousel-v3.js"
+const test = document.querySelector('#tea-station')
+const carousel = new c(["./assets/folio/CBR-cover.png", "./assets/folio/CBR-inner1.png"]);
+test.append(carousel.container);
+carousel.init();
 
